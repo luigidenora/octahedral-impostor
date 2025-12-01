dbd51f8b51b6839b65077a7034e2eab89ee3825a
Luigi Denora <34819271+luigidenora@users.noreply.github.com>
2025-12-01 19:01:21 +0000

    refactor: migliorata la gestione delle geometrie e aggiornati i materiali

--- examples/showcase2/world.ts
+++ examples/showcase2/world.ts
@@ -1,16 +1,42 @@
 import { InstancedMesh2 } from '@three.ez/instanced-mesh';
-import { load, Main, PerspectiveCameraAuto } from '@three.ez/main';
-import { simplifyGeometriesByError } from '@three.ez/simplify-geometry';
-import { ACESFilmicToneMapping, AmbientLight, BoxGeometry, Color, DirectionalLight, FogExp2, Material, Mesh, MeshLambertMaterial, PCFSoftShadowMap, RepeatWrapping, Scene, TextureLoader, Vector3 } from 'three';
-import { GLTFLoader, MapControls } from 'three/examples/jsm/Addons.js';
-import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+import {
+  get,
+  loadPending,
+  Main,
+  PerspectiveCameraAuto,
+  preload
+} from '@three.ez/main';
+import { simplifyGeometryByError } from '@three.ez/simplify-geometry';
+import {
+  ACESFilmicToneMapping,
+  AmbientLight,
+  BoxGeometry,
+  BufferGeometry,
+  Color,
+  DirectionalLight,
+  FogExp2,
+  Mesh,
+  MeshLambertMaterial,
+  MeshStandardMaterial,
+  PCFSoftShadowMap,
+  Scene,
+  Vector3
+} from 'three';
+import { GLTF, GLTFLoader, MapControls } from 'three/examples/jsm/Addons.js';
 import { OctahedralImpostor } from '../../src/core/octahedralImpostor.js';
 import { Terrain } from './terrain.js';
+
+const pineModelPath = 'showcase2/spruce_tree.glb';
+const terrainModelPath = 'showcase2/terrain-with-points.glb';
+preload(GLTFLoader, pineModelPath, terrainModelPath);
 
 const fogColor = 0xaec1c7;
 const camera = new PerspectiveCameraAuto(50, 0.1, 1200).translateY(5);
 const scene = new Scene();
-const main = new Main({ showStats: true, rendererParameters: { antialias: false } }); // init renderer and other stuff
+const main = new Main({
+  showStats: true,
+  rendererParameters: { antialias: false }
+}); // init renderer and other stuff
 
 main.renderer.toneMapping = ACESFilmicToneMapping;
 main.renderer.toneMappingExposure = 0.7;
@@ -24,8 +50,19 @@ controls.update();
 
 main.renderer.setPixelRatio(Math.min(1.25, window.devicePixelRatio));
 
-load(GLTFLoader, 'showcase2/pine.glb').then(async (gltf) => {
-  const mesh = gltf.scene;
+loadPending({
+  onProgress: (ratio) => {
+    setLoadingProgress(ratio * 100);
+  }
+}).then(async () => {
+  const gltf = get<GLTF>(pineModelPath);
+  const mesh = gltf.scene.children[0].children[0].children[0].children[0].children[0] as Mesh<
+    BufferGeometry,
+    MeshStandardMaterial
+  >;
+
+  // mesh.material.transparent = false;
+  // mesh.material.depthWrite = true;
 
   scene.background = new Color(fogColor);
 
@@ -54,21 +91,30 @@ load(GLTFLoader, 'showcase2/pine.glb').then(async (gltf) => {
 
   // TERRAIN
   const terrain = new Terrain();
+  terrain.renderOrder = -1; // this can be based on camera rotation
+  terrain.receiveShadow = true;
+  terrain.castShadow = true;
   scene.add(terrain);
 
   // TREES AND IMPOSTORS
 
-  const mergedGeo = mergeGeometries(mesh.children.map((x) => (x as Mesh).geometry), true);
-  const materials = mesh.children.map((x) => (x as Mesh).material as Material);
-
   const pos = await terrain.generateTrees();
 
-  const iMesh = new InstancedMesh2(mergedGeo, materials, { createEntities: true, renderer: main.renderer, capacity: pos.length });
+  const iMesh = new InstancedMesh2(mesh.geometry, mesh.material, {
+    renderer: main.renderer,
+    capacity: pos.length
+  });
+
+  mesh.material.metalness = 0;
+  mesh.material.roughness = 1;
+  mesh.material.alphaTest = 0.55;
 
   iMesh.addInstances(pos.length, (obj, index) => {
     obj.position.copy(pos[index]);
-    obj.rotateY(Math.random() * Math.PI * 2).rotateX(Math.random() * 0.5 - 0.25);
-    obj.scale.setScalar(Math.random() * 0.5 + 0.75);
+    obj
+      .rotateY(Math.random() * Math.PI * 2)
+      .rotateX(Math.random() * 0.5 - 0.25);
+    obj.scale.setScalar(Math.random() * 0.0001);
   });
 
   const impostor = new OctahedralImpostor({
@@ -77,23 +123,27 @@ load(GLTFLoader, 'showcase2/pine.glb').then(async (gltf) => {
     useHemiOctahedron: true,
     transparent: false,
     alphaClamp: 0.5,
-    spritesPerSide: 12,
+    spritesPerSide: 16,
     textureSize: 4096,
     baseType: MeshLambertMaterial
   });
 
-  const LODGeo = await simplifyGeometriesByError(mesh.children.map((x) => (x as Mesh).geometry), [0, 0.01]); // improve
-  const mergedGeoLOD = mergeGeometries(LODGeo, true);
-
-  iMesh.addLOD(mergedGeoLOD, mesh.children.map((x) => ((x as Mesh).material as Material).clone()), 5);
-  iMesh.addLOD(impostor.geometry, impostor.material, 50);
+  const LODGeo = await simplifyGeometryByError(mesh.geometry, 0.03);
+  iMesh.addLOD(LODGeo, mesh.material, 5);
+  iMesh.addLOD(impostor.geometry, impostor.material, 20);
   iMesh.addShadowLOD(new BoxGeometry(3, 10, 3));
   iMesh.computeBVH();
 
   scene.add(iMesh);
 
   main.createView({ scene, camera, enabled: false });
-
   document.getElementById('loading').remove();
+
   document.getElementById('info').style.display = 'block';
 });
+
+function setLoadingProgress(pct: number): void {
+  const clamped = Math.max(0, Math.min(100, pct));
+  const pb = document.getElementById('progress-bar') as HTMLDivElement | null;
+  if (pb) pb.style.width = `${clamped}%`;
+}
